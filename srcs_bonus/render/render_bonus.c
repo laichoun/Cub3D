e7cdc468fa704494acd70222f421002052f9169f
Pierre Bernard <pibernar@student.42luxembourg.lu>
2024-12-12 18:44:41 +0100

    some tests to slow down rendering

--- srcs_bonus/render/render_bonus.c
+++ srcs_bonus/render/render_bonus.c
@@ -11,20 +11,26 @@
 /* ************************************************************************** */
 
 #include "../../includes/cub3d_bonus.h"
+#include <stdio.h>
 
 void	render_startscreen(t_game *game);
 
 int	render(t_game *game)
 {
+	static int	frame_count = 0;
+
+	frame_count++;
 	if (game->state == 0)
 		render_startscreen(game);
-	else
+	else if (frame_count % 50 == 0)
 	{
 		render_raycast(game);
 		draw_minimap(game);
 		render_animation(game);
 		player_actions(game);
 		mlx_put_image_to_window(game->mlx, game->win, game->screen, 0, 0);
+		printf("%d\n", frame_count);
+		frame_count = 0;
 	}
 	return (SUCCESS);
 }
