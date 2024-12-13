b90edfa40927293364f62d4322c8338ac50bc92d
Pierre Bernard <pibernar@student.42luxembourg.lu>
2024-12-13 19:00:22 +0100

    fps done

--- srcs_bonus/render/render_bonus.c
+++ srcs_bonus/render/render_bonus.c
@@ -14,12 +14,11 @@
 #include <stdio.h>
 
 void	render_startscreen(t_game *game);
+void	fps(t_game *game, struct timeval cur);
 
 int	render(t_game *game)
 {
 	static struct timeval	last;
-	static struct timeval	fps;
-	static int				count = 0;
 	struct timeval			cur;
 
 	if (gettimeofday(&cur, NULL) == -1)
@@ -32,15 +31,31 @@ int	render(t_game *game)
 		draw_minimap(game);
 		render_animation(game);
 		player_actions(game);
-		if ((cur.tv_sec - fps.tv_sec) * 1000 + (cur.tv_usec - fps.tv_usec) / 1000 >= 1000)
-		{
-			printf("%d\n", count);
-			count = 0;
-			fps = cur;
-		}
 		mlx_put_image_to_window(game->mlx, game->win, game->screen, 0, 0);
+		fps(game, cur);
 		last = cur;
-		++count;
 	}
 	return (SUCCESS);
+}
+
+void	fps(t_game *game, struct timeval cur)
+{
+	static struct timeval	fps;
+	static int				count = 0;
+	static int				last_count = 0;
+	char					*num;
+
+	if ((cur.tv_sec - fps.tv_sec) * 1000 + (cur.tv_usec - fps.tv_usec) / 1000 >= 1000)
+	{
+		last_count = count;
+		count = 0;
+		fps = cur;
+	}
+	num = ft_itoa(last_count);
+	if (!num)
+		return (void)0;
+	mlx_set_font(game->mlx, game->win, "-misc-fixed-bold-r-normal--18-120-100-100-c-90-iso8859-9");
+	mlx_string_put(game->mlx, game->win, 0, 15, 0xFFFF00, num);
+	++count;
+	free(num);
 }
